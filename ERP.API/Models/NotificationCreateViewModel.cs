e454e5e103fd8acd6c5a7caa6f3b3ebbb16797ee
openupmta <openupmta99@gmail.com>
2020-01-20 15:12:15 +0700

    notifications

---
+++ ERP.API/Models/NotificationCreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP.API.Models
+{
+    public class NotificationCreateViewModel
+    {
+        public NotificationCreateViewModel() { }
+        
+
+        [StringLength(50)]
+        public string ntf_title { get; set; }
+
+        public string ntf_description { get; set; }
+
+        public DateTime? ntf_datetime { get; set; }
+
+        public DateTime? ntf_confim_datetime { get; set; }
+
+        public int? staff_id { get; set; }
+    }
+}
